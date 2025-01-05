9e416b220c0af6175a930c31e1e87e3075a65b48
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-05 04:56:00 -0600

    finish project

---
+++ scripts.js
@@ -0,0 +1,74 @@
+console.log("Hello, World!")
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    const number = Math.random();
+    console.log(number)
+
+    if (number <= .33) {
+        return "rock"
+    } else if (number <= .66) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+function getHumanChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let choice = prompt("Choice: Rock, Paper, Scissors").toLowerCase();
+
+    if (options.includes(choice)) {
+        return choice
+    }
+    else getHumanChoice()
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Draw")
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock")
+        computerScore++;
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats scissors")
+        humanScore++;
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats paper")
+        computerScore++;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats rock")
+        humanScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats scissors")
+        computerScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats paper")
+        humanScore++;
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice())
+        console.log(computerScore, humanScore) 
+    }
+}
+
+playGame()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
